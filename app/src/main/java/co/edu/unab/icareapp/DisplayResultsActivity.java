f1d976f4ca64ee6a580168a9e2f60f7b7807737f
Sergio Pachon Dotor <sap9827@gmail.com>
2021-11-10 13:47:17 -0500

    Sprint 2

--- app/src/main/java/co/edu/unab/icareapp/DisplayResultsActivity.java
+++ app/src/main/java/co/edu/unab/icareapp/DisplayResultsActivity.java
@@ -2,7 +2,10 @@ package co.edu.unab.icareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class DisplayResultsActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class DisplayResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_results);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String resImc = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String resMbs = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView resultado_imc_tv = findViewById(R.id.resultado_imc);
+        TextView resultado_mbs_tv = findViewById(R.id.resultado_metabolismobasal);
+
+        resultado_imc_tv.setText(resImc);
+        resultado_mbs_tv.setText(resMbs);
     }
 }
--- app/src/main/java/co/edu/unab/icareapp/MainActivity.java
+++ app/src/main/java/co/edu/unab/icareapp/MainActivity.java
@@ -2,6 +2,7 @@ package co.edu.unab.icareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "co.edu.unab.icareapp.MESSAGE";
     EditText edad;
     EditText peso;
     EditText altura;
@@ -23,24 +25,29 @@ public class MainActivity extends AppCompatActivity {
         peso = (EditText) findViewById(R.id.peso);
         altura = (EditText) findViewById(R.id.altura);
         genero = (Spinner) findViewById(R.id.SpinnerFeedbackType);
-        calculo = (Button)findViewById(R.id.boton_calculo);
-
-        calculo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                double imc_final = calcularimc();
-                String mbasal_final = calcularmbasal();
-            }
-        });
-
+        calculo = (Button) findViewById(R.id.boton_calculo);
     }
 
+        public void sendResults(View view) {
+            String imc_final = calcularimc();
+            String mbasal_final = calcularmbasal();
+            Intent intent = new Intent(this, DisplayResultsActivity.class);
+            // TextView editTextMetBas = (TextView) findViewById(R.id.resultado_metabolismobasal);
+            // TextView editTextImc = (TextView) findViewById(R.id.resultado_imc);
 
-    public double calcularimc(){
+            intent.putExtra(EXTRA_MESSAGE, imc_final);
+            intent.putExtra(EXTRA_MESSAGE, mbasal_final);
+            startActivity(intent);
+            }
+
+
+
+    public String calcularimc(){
         double peso_d = Double.parseDouble(peso.getText().toString());
         double altura_d = Double.parseDouble(altura.getText().toString());
         double imc_d  = peso_d / altura_d * altura_d;
-        return imc_d;
+        String resultado = "Su IMC es: "+imc_d;
+        return resultado;
     }
 
     public String calcularmbasal(){
@@ -51,8 +58,16 @@ public class MainActivity extends AppCompatActivity {
         double mbasal_mujer =( 10*peso_d)+(6.25*altura_d*100)-(5*edad_d)-161;
         double mbasal_hombre = ( 10*peso_d)+(6.25*altura_d*100)-(5*edad_d)+5;
 
-        String resultado = "El metabolismo basal para una mujer es: "+mbasal_mujer+" y para un hombre es: "+mbasal_hombre;
-        return resultado;
+        if (genero.getSelectedItem().toString() == "Masculino"){
+            String resultado_h = "Su metabolismo basal es: "+mbasal_hombre;
+            return resultado_h;
+        }
+        else if(genero.getSelectedItem().toString() == "Femenino"){
+            String resultado_m = "Su metabolismo basal es: "+mbasal_mujer;
+            return resultado_m;
+        }
+        String mensaje = "Algo falló";
+        return mensaje;
     }
 
 }
